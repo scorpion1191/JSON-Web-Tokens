159bb17313efc587ed08a376ee78aaa7280138af
subbu <subbu1191@gmail.com>
2019-03-10 21:01:30 +0530

    JSON-WEB-TOKEN

--- routes/users/userRoutes.ts
+++ routes/users/userRoutes.ts
@@ -3,8 +3,7 @@ import User from '../../models/users/userModel'
 import {authController} from "../../controllers/security/authController"
 
 export class userRoutes {       
-    public routes(app): void {  
-        // RETURNS ALL THE USERS IN THE DATABASE        
+    public routes(app): void {     
         app.route('/verification').post(authController.verifyApiToken,(req:Request, res:Response) => {
             res.status(200).send({
                 message: 'API authentication successfully done'
